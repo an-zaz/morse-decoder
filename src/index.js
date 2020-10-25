20581c1f19cdd89a396a86de483927d0edeeea34
Nastya <an_zazerskaya@mail.ru>
2020-10-25 22:00:07 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -37,10 +37,35 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const MORSE_SYMBOLS = {
+    '10': '.',
+    '11': '-',
+};
+
+const WORD_SIZE = 10;
+
 function decode(expr) {
-    // write your solution here
+    let decodedWord = '';
+    for (let i = 0; i < expr.length / WORD_SIZE; i++) {
+        let currentWord = expr.slice(i * WORD_SIZE, (i + 1) * WORD_SIZE);
+
+        if (currentWord === '**********') {
+            decodedWord += ' ';
+            continue;
+        }
+        let morseWord = '';
+        for (let j = 0; j < 10 / 2; j++) {
+            let morseSymbol = currentWord.slice(j * 2, (j + 1) * 2);
+            if (morseSymbol === '00') {
+                continue;
+            }
+            morseWord += MORSE_SYMBOLS[morseSymbol];
+        }
+        decodedWord += MORSE_TABLE[morseWord];
+    }
+    return decodedWord;
 }
 
 module.exports = {
     decode
-}
+};
